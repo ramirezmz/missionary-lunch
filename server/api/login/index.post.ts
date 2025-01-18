ac2de3acd1a05f4410be6664db6c78b09707dfef
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2025-01-18 18:25:38 -0300

    chore: integrate Prisma for database access and add login API endpoint with validation

---
+++ server/api/login/index.post.ts
@@ -0,0 +1,51 @@
+import { z } from "zod"
+import prisma from "~/lib/prisma"
+import { comparePassword } from "~/server/services/hashedPassword"
+
+const LoginSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z.string({
+    message: "Password is required",
+  }),
+})
+
+export default defineEventHandler(async (event) => {
+  const content = await readValidatedBody(event, LoginSchema.parse)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: content.email,
+    },
+  })
+
+  if (!user) {
+    return {
+      status: 404,
+      body: {
+        message: "User not found",
+      },
+    }
+  }
+
+  const passwordMatch = comparePassword(content.password, user.password)
+
+  if (!passwordMatch) {
+    return {
+      status: 401,
+      body: {
+        message: "Invalid password",
+      },
+    }
+  }
+  
+  return {
+    status: 200,
+    body: {
+      message: "Login successful",
+      data: user,
+    },
+  }
+
+})
