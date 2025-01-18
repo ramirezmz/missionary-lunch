52d5cfdcb393b14190eb76823c0a0d9c0831c4bf
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2025-01-18 19:56:15 -0300

    chore: add token verification function to enhance JWT handling

--- server/services/token.ts
+++ server/services/token.ts
@@ -4,3 +4,8 @@ export function generateToken(userId: string) {
   const runtimeConfig = useRuntimeConfig();
   return jwt.sign({ userId }, runtimeConfig.apiSecret.development.jwt_secret);
 }
+
+export function verifyToken(token: string) {
+  const runtimeConfig = useRuntimeConfig();
+  return jwt.verify(token, runtimeConfig.apiSecret.development.jwt_secret);
+}
