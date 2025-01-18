0ce3f4e511be64baf72e807b978084da033cc027
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2025-01-17 21:05:00 -0300

    start project

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert> This is an auto-imported component. </AppAlert>
+    <pre>{{ data }}</pre>
+  </div>
+</template>
+<script setup lang="ts">
+const { data } = await useFetch("/api/hello");
+console.log("data: ", data.value);
+</script>
